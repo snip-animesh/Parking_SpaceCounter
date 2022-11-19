b888e9404eb847fe7e3d4c694eaf204daee84a55
snip-animesh <animesh12126@gmail.com>
2022-11-19 13:22:11 +0600

    new video added

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ kernel = np.ones((3, 3), np.uint8)
 GREEN = (0, 255, 0)
 RED = (0, 0, 255)
 CAR_PIXEL = 800
+bgWidth , bgHeight = 1280 , 720
 
 
-def check_parking_space(imgPro):
+def check_parking_space(imgPro,imgBG):
     spaceCounter = 0
 
     for pos in posList:
@@ -39,6 +40,9 @@ def check_parking_space(imgPro):
 
     cvzone.putTextRect(img, f'Free: {str(spaceCounter)}/{len(posList)}', (100, 50), scale=2,
                        thickness=3, offset=10, colorR=RED)
+    # Putting counted value on background image
+    cv2.putText(imgBG, str(spaceCounter), (1101, 235), cv2.FONT_ITALIC,1,RED,thickness=3)
+    cv2.putText(imgBG, str(len(posList)), (1101, 175), cv2.FONT_ITALIC,1,GREEN,thickness=3)
 
 
 while True:
@@ -48,6 +52,12 @@ while True:
 
     success, img = cap.read()
 
+
+    # importing background Image
+    imgBackground = cv2.imread('Resources/background.png')
+    # resize the image
+    imgBackground = cv2.resize(imgBackground,(bgWidth,bgHeight),interpolation=cv2.INTER_LINEAR)
+
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
     imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
@@ -56,9 +66,13 @@ while True:
 
     imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
 
-    check_parking_space(imgDilate)
+    check_parking_space(imgDilate,imgBackground)
 
-    cv2.imshow("Image", img)
+    # Placing CCTV footage to background image
+    imgResize = cv2.resize(img, (695,341)) # width & height
+    imgBackground[178:519,47:742] = imgResize # height & width
+
+    cv2.imshow("User Interface", imgBackground)
     # cv2.imshow("ImageDilate", imgDilate)
     K = cv2.waitKey(10)
     if K == ord('q'):
